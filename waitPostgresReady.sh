a802cdd15ff62b9122c9ccab2e7841290ef18269
papa <ivanov2000@bk.ru>
2020-05-23 16:28:09 +0700

    added pauses

--- waitPostgresReady.sh
+++ waitPostgresReady.sh
@@ -1,8 +1,8 @@
  #!/bin/bash 
-         COUNTER = 0
-         while [$COUNTER -lt 2]; do
+         COUNTER=0
+         while [ $COUNTER -lt 2 ]; do
+		sleep 10s
+             COUNTER=$(docker logs postgres 2>&1|grep -c 'database system is ready to accept connections')
              echo "The counter is $COUNTER"
-             COUNTER = $(docker logs postgres 2>&1|grep -c 'database system is ready to accept connections')
-		echo $?
          done
          
