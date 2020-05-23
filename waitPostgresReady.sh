4fa27846d97d5573393320efbd8e5a469e5e7e75
papa <ivanov2000@bk.ru>
2020-05-23 16:11:42 +0700

    tryed to form COUNTER

--- waitPostgresReady.sh
+++ waitPostgresReady.sh
@@ -1,7 +1,7 @@
  #!/bin/bash 
          COUNTER = 0
          while [$COUNTER -lt 2]; do
-             echo The counter is $COUNTER
+             echo "The counter is $COUNTER"
              COUNTER = $(docker logs postgres 2>&1|grep -c 'database system is ready to accept connections')
 		echo $?
          done
