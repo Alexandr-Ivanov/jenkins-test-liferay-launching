d1543b585a45eec51fd1fee59dce51fa59578188
papa <ivanov2000@bk.ru>
2020-05-24 15:52:21 +0700

    Added waiting Catalina to start.

--- startLiferay.sh
+++ startLiferay.sh
@@ -1,1 +1,9 @@
 docker run -dit -v ~/liferay/logs:/opt/liferay/logs -v ~/liferay/tomcat/logs:/opt/liferay/tomcat-7.0.62/logs -v ~/liferay/deploy:/opt/liferay/deploy -v ~/liferay/keys:/var/opt/cprocsp/keys/root --name liferay --publish 8000:8080 liferay:1.0
+
+COUNTER=0
+
+while [ $COUNTER -lt 1 ]; do
+	sleep 30s
+        COUNTER=$(docker logs liferay 2>&1|grep -c 'Catalina start')
+        echo "The counter is $COUNTER"
+done
